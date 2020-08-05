1558e1f22d9a2461414ac82cf4aef980dab9a99c
sairaja9 <saikraja9@gmail.com>
2020-08-05 00:22:11 -0700

    Site created and styled, JS not written

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 alert("This page is under development");
+
+/* function search(){
+    var search = document.getElementById("searchBar").value;
+    var searchRange = document.getElementsByClassName("year");
+
+    for (item of searchRange.values()){
+
+    }
+} */
