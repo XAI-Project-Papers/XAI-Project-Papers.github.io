3955885577522327fc454ae7e9686dd0498d9200
sairaja9 <saikraja9@gmail.com>
2020-08-05 14:23:35 -0700

    view filters added

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-alert("This page is under development");
+//alert("This page is under development");
+
+function lists(){
+    var panels = document.getElementById("panels");
+    var lists = document.getElementById("lists");
+    panels.style.display = "none";
+    lists.classList.remove("hidden")
+}
+
+function panels(){
+    var panels = document.getElementById("panels");
+    var lists = document.getElementById("lists");
+    panels.style.display = "grid";
+    lists.classList.add("hidden");
+}
 
 /* function search(){
     var search = document.getElementById("searchBar").value;
