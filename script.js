faf1a6cd27c9ae1f285a7786023a48b75787493c
sairaja9 <saikraja9@gmail.com>
2020-08-05 17:50:12 -0700

    working on search function

--- script.js
+++ script.js
@@ -14,11 +14,48 @@ function panels(){
     lists.classList.add("hidden");
 }
 
-/* function search(){
-    var search = document.getElementById("searchBar").value;
-    var searchRange = document.getElementsByClassName("year");
-
-    for (item of searchRange.values()){
-
+function enter(event){
+    if (event.keyCode == 13 || event.which == 13){
+        search();
     }
-} */
+}
+
+function search(){
+    var searchInput = document.getElementById("searchBar").value;
+    var search = searchInput.toUpperCase();
+
+    var h2 = document.getElementsByClassName("pubTitle");
+    var p = document.getElementsByClassName("pubDes");
+    
+    for (i=0; i < h2.length; i++){
+        var valH2 = h2[i].innerText.toUpperCase();
+        if (valH2.indexOf(search)>-1){
+            h2[i].style.zIndex = '10000000';
+            h2[i].style.backgroundColor = 'yellow'
+        }
+        else{
+            var item = h2[i];
+            findAndHideParentPanel(item);
+        }
+    }
+}
+
+function findAndHideParentPanel(innerObject){
+    var object = innerObject;
+    var validation = false;
+    while(!validation){
+        //alert("works");//This alert shows up!!!!!!!!!!
+        if (object.classList.indexOf(panel) > -1){
+            break;
+            alert("works");//This alert does not show up
+            validation = true;
+        }
+        else{
+            break;
+            alert("works");//This alert does not show up
+            object = object.parentNode;
+        }
+    }
+    alert("works");//This still doesnt shoew up !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    object.classList.add("hidden");
+}
